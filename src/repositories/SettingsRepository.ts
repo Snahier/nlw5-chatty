76e97121b13e84dbf02c7973f6e826c77f6b176f
Snahier <hansalmeida@hotmail.com>
2021-04-20 21:43:02 -0300

    Add repository missing property

--- src/repositories/SettingsRepository.ts
+++ src/repositories/SettingsRepository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from "typeorm"
 import { Setting } from "../entities/Setting"
 
-@EntityRepository()
+@EntityRepository(Setting)
 export class SettingsRepository extends Repository<Setting> {}
