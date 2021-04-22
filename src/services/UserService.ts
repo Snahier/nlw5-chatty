b8739473f549de2279514dac3ace163328207319
Snahier <hansalmeida@hotmail.com>
2021-04-21 21:18:06 -0300

    Create user service

---
+++ src/services/UserService.ts
@@ -0,0 +1,20 @@
+import { getCustomRepository } from "typeorm"
+import { UserRepository } from "../repositories/UserRepository"
+
+export class UserService {
+  async create(email: string) {
+    const usersRepository = getCustomRepository(UserRepository)
+
+    const user = await usersRepository.findOne({ email })
+
+    if (user) return user
+
+    const newUser = usersRepository.create({
+      email,
+    })
+
+    await usersRepository.save(user)
+
+    return user
+  }
+}
