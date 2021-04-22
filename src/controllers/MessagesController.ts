04db7a53e871f0379f9df75fc258c69bca4f07ec
Snahier <hansalmeida@hotmail.com>
2021-04-21 21:46:16 -0300

    Create messages controller

---
+++ src/controllers/MessagesController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express"
+import { MessagesService } from "../services/MessagesService"
+
+export class MessagesController {
+  async create(request: Request, response: Response) {
+    const { admin_id, text, user_id } = request.body
+
+    const messageService = new MessagesService()
+
+    const message = await messageService.create({
+      admin_id,
+      text,
+      user_id,
+    })
+
+    return response.json(message)
+  }
+}
