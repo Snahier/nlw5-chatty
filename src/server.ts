10f9546f5c202a047d0c046248164bed34995fc5
Snahier <hansalmeida@hotmail.com>
2021-04-22 21:05:34 -0300

    Create socket io instance

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import express from "express"
+import { createServer } from "http"
+import { Server, Socket } from "socket.io"
 import "./database"
 import { routes } from "./routes"
 
 const app = express()
 
+const http = createServer(app) // Create HTTP server
+const io = new Server(http) // Create WebSocket server
+
+io.on("connection", (socket: Socket) => {
+  console.log("Connected", socket.id)
+})
+
 // Express middlewares
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => console.log("listening on http://localhost:3333"))
+http.listen(3333, () => console.log("listening on http://localhost:3333"))
