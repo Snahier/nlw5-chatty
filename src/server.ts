9afc261964cdb3560b469181cd16afe640ad9cef
Snahier <hansalmeida@hotmail.com>
2021-04-22 21:09:41 -0300

    Add HTML link

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express"
 import { createServer } from "http"
+import path from "path"
 import { Server, Socket } from "socket.io"
 import "./database"
 import { routes } from "./routes"
 
 const app = express()
+app.use(express.static(path.join(__dirname, "..", "public")))
+app.set("views", path.join(__dirname, "..", "public"))
+app.engine("html", require("ejs").renderFile)
+app.set("view engine", "html")
 
 const http = createServer(app) // Create HTTP server
 const io = new Server(http) // Create WebSocket server
