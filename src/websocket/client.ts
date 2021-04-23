af188af3e97d11b798f7344fe5f9f0f059c96e47
Snahier <hansalmeida@hotmail.com>
2021-04-22 21:45:28 -0300

    Add first access socket

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,6 +1,7 @@
-import { Socket } from "socket.io"
 import { io } from "../http"
 
-io.on("connection", (socket: Socket) => {
-  console.log("Se conectou", socket.id)
+io.on("connect", (socket) => {
+  socket.on("client_first_access", (params) => {
+    console.log(params)
+  })
 })
